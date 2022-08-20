828de3f1a6286eccfa57d33838473f72e74d4553
Deny Nakano <deny.nakano@gmail.com>
2022-08-20 13:06:50 -0300

    project done

--- script/index.js
+++ script/index.js
@@ -10,7 +10,8 @@ const gameArea = {
     canvas: document.getElementById('canvas'),
     frames: 0,
     obstacles: [],
-    //stop: false,
+    powers: [],
+    score: 0,
     start: function () {
         this.canvas.width = 800;
         this.canvas.height = 500;
@@ -21,6 +22,14 @@ const gameArea = {
     },
     clear: function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    },
+
+    setScore: function () {
+        const points = gameArea.score;
+        this.context.font = '60px serif';
+        this.context.fillStyle = 'black';
+        this.context.fillText(`Score: ${points}`, 350, 50);
+
     }
 }
 
@@ -52,10 +61,7 @@ class Knight{
         this.y += 50
     }
     }
-    /*crashWith(obstacles){
-        return false
-    }*/
-
+    
 }
 
 class Obstacle{
@@ -72,10 +78,68 @@ class Obstacle{
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
         
     }
-    updatePosition() {
+    updatePosition(){
         this.x -= 1
+        if(this.x <= 0){
+            gameOver()
+        }
+    }
+
+    top (){
+        return this.y
+    }
+    bottom (){
+        return this.y + this.height
+    }
+    left (){
+        return this.x
+    }
+    right (){
+        return this.x + this.width
+    }
+    
+    crashWith(power){
+        return !(
+            this.bottom() < power.top()
+            || this.top() > power.bottom()
+            || this.right() < power.left()
+            || this.left() > power.right()
+        )
     }
 }
+
+class Power{
+    constructor(y){
+      this.x = 50;
+      this.y = y;
+      this.width = 50;
+      this.height = 50;
+      this.image = new Image()
+      this.image.src = './images/meteoro.png'
+    }
+  
+    draw(){
+      const ctx = gameArea.context
+      ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
+    }
+  
+    updateThrowPower(){
+      this.x += 1
+    }
+    
+    top (){
+        return this.y
+    }
+    bottom (){
+        return this.y + this.height
+    }
+    left (){
+        return this.x
+    }
+    right (){
+        return this.x + this.width
+    }
+  }
 //const arrow = new Obstacle(250)   
 
 const pegasus = new Knight()
@@ -89,15 +153,22 @@ const startWalking = () => {
 
 
 document.addEventListener('keydown', (e) => {
+
     const key = e.code
     if (key === 'ArrowUp'){
         pegasus.moveUp()
+        e.preventDefault()
     }
     if (key === 'ArrowDown'){
         pegasus.moveDown()
+        e.preventDefault()
+    }
+    if (key === 'ArrowRight') {
+        throwPower()
     }
     
 })
+
 function createObstacle() {
     let minHeight = 30
     let maxHeight = 450 
@@ -107,22 +178,52 @@ function createObstacle() {
     gameArea.obstacles.push(arrow)
 }
 
-function checkGameOver(){
-    const crashed = gameArea.obstacles.some(Obstacle => pegasus.crashWith (Obstacle))
-    if (crashed) {
-        gameArea.stop = true
+function throwPower() {
+    console.log(pegasus.y)
+    const power = new Power(pegasus.y)
+  
+    gameArea.powers.push(power)
+    updateThrowPower()
+  }
+  
+  function updateThrowPower() {
+    for (power of gameArea.powers) {
+      power.updateThrowPower()
+      power.draw()
     }
+  }
+function checkScore(){
+    let crashed = false
+    for (let i = 0; i < gameArea.powers.length; i += 1) {
+        for (let j = 0; j < gameArea.obstacles.length; j += 1) {
+            crashed = gameArea.obstacles[j].crashWith(gameArea.powers[i])
+            console.log(crashed)
+            if (crashed){
+                gameArea.obstacles.splice(j, 1)
+                gameArea.powers.splice(i, 1)
+                gameArea.score += 1
+                break
+            }
+        }
+    }
+    
 }
-
+function gameOver() {
+    gameArea.stop = true
+}
 
 function updateGameArea() {
     gameArea.clear()
     pegasus.draw()
     updateObstacle()
+    updateThrowPower()
+    checkScore()
     gameArea.frames += 1
+    gameArea.setScore()
     if (!gameArea.stop){
     requestAnimationFrame(updateGameArea)
-    
+} else {
+    cancelAnimationFrame(updateGameArea)
 }
 
 function updateObstacle() {
